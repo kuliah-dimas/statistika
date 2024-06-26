9fa7799ac7ee9b736ab2c9b51b9411ccb3b544df
Dimas Febriyanto <dimassfeb@gmail.com>
2024-06-26 09:02:10 +0700

    ACT3

--- M3/ACT3.java
+++ M3/ACT3.java
@@ -1,7 +1,7 @@
-public class PersamaanRegresi {
+public class ACT3 {
     public static void main(String[] args) {
-        double[] x = { 70, 60, 80, 90, 50, 65 };
-        double[] y = { 175, 160, 180, 185, 155, 170 };
+        double[] x = { 1.5, 1.0, 2.0, 2.8, 0.4, 1.3 };
+        double[] y = { 3.6, 2.8, 4.3, 5.4, 1.9, 2.9 };
         double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0, sumY2 = 0;
 
         int n = x.length;
@@ -25,15 +25,15 @@ public class PersamaanRegresi {
         System.out.println(a);
         System.out.println(b);
 
-        System.out.printf("a = %f, b = %f\n", a, b);
+        System.out.printf("a = %.2f, b = %.2f", a, b);
 
-        double x2 = 75;
+        double x2 = 10;
         double langkah4 = a + b * x2;
 
-        System.out.printf("langkah4: %f\n", langkah4);
+        System.out.printf("%.2f\n", langkah4);
 
         double kesalahanaRegresi = (sumY2 - a * sumY - b * sumXY) / (n - 2);
         double akarKesalahan = Math.sqrt(kesalahanaRegresi);
-        System.out.printf("%f\n", akarKesalahan);
+        System.out.printf("%.2f\n", akarKesalahan);
     }
 }
